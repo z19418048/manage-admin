5045543a596b5b18266c34107507b5a26e44e75a
z19418048 <953829746@qq.com>
2022-11-25 16:13:30 +0800

    add

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import TDesign from "tdesign-vue";
+import store from "@/store/index.js";
 // 引入组件库全局样式资源
 import "tdesign-vue/dist/reset.css";
+
 import "tdesign-vue/es/style/index.css";
 
 Vue.config.productionTip = false;
@@ -13,5 +15,6 @@ process.env.NODE_ENV === "development" && require("./mock");
 
 new Vue({
   router,
+  store,
   render: (h) => h(App),
 }).$mount("#app");
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,21 @@
 import Vue from "vue";
-import Vuex from "vuex";
+import Vuex, { createLogger } from "vuex";
 import { actions } from "./actions";
 import { mutations } from "./mutations";
 
 Vue.use(Vuex);
+
+const debug = process.env.NODE_ENV !== "production";
+
+//遍历模块
+const modules = {};
+const files = require.context("./modules", false, /\.js$/);
+files.keys().forEach((key) => {
+  modules[key.replace(/(modules|\/|\.|js)/g, "")] = {
+    ...files(key).default,
+    namespcae: true,
+  };
+});
 
 const state = () => ({ tolen: null });
 
@@ -11,4 +23,7 @@ export default new Vuex.Store({
   state,
   actions,
   mutations,
+  modules,
+  strict: debug,
+  plugins: debug ? [createLogger()] : [],
 });
